8d565d6fdc992d2526214a41ec66e86c26fded27
riku-pon <68589131+riku-pon@users.noreply.github.com>
2020-08-06 16:07:27 +0900

    Basic認証の修正１

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do | username, password |
-      username == 'rikupon' && password == '109315'
+      username == '' && password == ''
     end
   end
 end

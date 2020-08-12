4771e33ec26c13013a8e9a4e9afd1f59317fdc0a
riku-pon <68589131+riku-pon@users.noreply.github.com>
2020-08-12 17:22:39 +0900

    userモデルの単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  with_options presence: true do
+    validates :family_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
+    validates :phonetic_family_name, :phonetic_first_name, format: { with:/\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
+    validates :nickname, :birthday
+  end
+  validates :password, format: { with: /\A[a-zA-Z0-9]+\z/, message: "Include both letters and numbers"}
 end

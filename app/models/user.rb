4076c53a6467b0785fefefb6376070e84c3a3122
riku-pon <68589131+riku-pon@users.noreply.github.com>
2020-08-13 21:28:17 +0900

    userモデルの単体テストコードの修正4

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+    FULL_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze
+    FULL_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
+    PASSWORD_REGEX = /\A[a-zA-Z0-9]+\z/.freeze
+
   with_options presence: true do
-    validates :family_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
-    validates :phonetic_family_name, :phonetic_first_name, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
+    validates :family_name, :first_name, format: { with: FULL_NAME_REGEX, message: 'Full-width characters' }
+    validates :phonetic_family_name, :phonetic_first_name, format: { with: FULL_NAME_KANA_REGEX, message: 'Full-width katakana characters' }
     validates :nickname, :birthday
   end
   validates_uniqueness_of :nickname, case_sensitive: true
-  validates :password, format: { with: /\A[a-zA-Z0-9]+\z/, message: 'Include both letters and numbers' }
+  validates :password, format: { with: PASSWORD_REGEX, message: 'Include both letters and numbers' }
 end

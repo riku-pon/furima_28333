50be900c296a2ec511fb270e6d9eb7fc34084051
riku-pon <68589131+riku-pon@users.noreply.github.com>
2020-08-21 10:23:36 +0900

    商品一覧ページの修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_action :find_item, only: :order
 
   def index
-    @items = Item.all
+    @items = Item.all.order("created_at DESC")
   end
 
   def new
